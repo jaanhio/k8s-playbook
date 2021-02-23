b787b36436619ff58687ae971ba9a555c90e3b69
Jianhao Tan <jianhaot.89@gmail.com>
2021-02-23 17:04:19 +0800

    chore: updated logic for identifying correct cpu architecture

--- scripts/get-architecture.py
+++ scripts/get-architecture.py
@@ -6,16 +6,19 @@ def is_amd64(name):
     amd64_namings = ['x86_64', 'x64', 'x86-64', 'amd64', 'intel 64']
     return name in amd64_namings
 
+def is_aarch64(name):
+    return name == 'aarch64'
+
 def get_arm_version_number(name):
     return name[4]
 
-def is_arm(name):
+def is_armv(name):
     return 'armv' in name
 
 if is_amd64(ansible_detected_machine_arch):
     print('amd64')
 
-if is_arm(ansible_detected_machine_arch):
+if is_armv(ansible_detected_machine_arch):
     
     version_number = get_arm_version_number(ansible_detected_machine_arch)
 
@@ -23,3 +26,7 @@ if is_arm(ansible_detected_machine_arch):
         print('armhf')
     else:
         print('arm64')
+
+# not sure what is the best way to identify the different architecture
+if is_aarch64(ansible_detected_machine_arch):
+    print('arm64')
